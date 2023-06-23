2d9620188020c52230856f4d50993128757de7c3
Arnavop <arnavkamramalout@gmail.com>
2023-06-23 15:38:40 +0530

    added footer and contact us

---
+++ script.js
@@ -0,0 +1,71 @@
+const saluteTextContainer = document.querySelector(".saluteTextContainer");
+const saluteTextContainer2 = document.querySelector(".saluteTextContainer2");
+const ourName = document.querySelector(".name");
+const jobTitles = document.querySelectorAll(".jobTitle");
+const projectTitle = document.querySelector(".projectTitle");
+const phone = document.querySelector(".phone");
+const laptop = document.querySelector(".laptop");
+
+const inputs = document.querySelectorAll(".input");
+const discordBtn = document.querySelector(".discord-btn"); // Add dot before "discord-btn"
+window.addEventListener("scroll", () => {
+  let offsetY = window.scrollY;
+  saluteTextContainer.style.transform = `translateY(${offsetY * 0.3}px)`;
+  saluteTextContainer2.style.transform = `translate(${offsetY * 0.2}px, ${offsetY * 0.3}px)`;
+  ourName.style.transform = `translateX(${offsetY * 0.2}px)`;
+  //add unique background transform for a unique background in jobtitlecontainer//
+  jobTitles[0].style.transform = `translateX(calc(200vh - ${offsetY * 0.9}px))`;
+  jobTitles[1].style.transform = `translateX(calc(-300vh + ${offsetY}px))`;
+  projectTitle.style.transform = `translateY(calc(400vh - ${offsetY}px))`;
+  phone.style.transform = `translateX(calc(500vh - ${offsetY}px))`;
+  laptop.style.transform = `translateX(calc(600vh - ${offsetY}px))`;
+});
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+discordBtn.addEventListener("click", function () {
+  window.location.href = "https://discord.com/invite/vibr-support-939509053623795732";
+});
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky");
+    }
+
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass("show");
+    } else {
+      $('.scroll-up-btn').removeClass("show");
+    }
+  });
+  $('.navbar .menu li a').click(function(){
+    // applying again smooth scroll on menu items click
+    $('html').css("scrollBehavior", "smooth");
+  });
+
+// toggle menu/navbar script
+  $('.menu-btn').click(function(){
+      $('.navbar .menu').toggleClass("active");
+      $('.menu-btn i').toggleClass("active");
+  });
+});
